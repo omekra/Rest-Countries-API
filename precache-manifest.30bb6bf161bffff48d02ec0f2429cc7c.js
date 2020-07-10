d4ba91bf36820cffd750676c0781676bad3d9546
Fued Kraide <Fued@Fueds-MacBook-Pro-2.local>
2020-07-10 14:27:12 +1000

    Updates

---
+++ precache-manifest.30bb6bf161bffff48d02ec0f2429cc7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed24c43735861a02e90699487d063dee",
+    "url": "/Rest-Countries-API/index.html"
+  },
+  {
+    "revision": "dca5919db099b2822944",
+    "url": "/Rest-Countries-API/static/css/main.318d2290.chunk.css"
+  },
+  {
+    "revision": "7f0743570d2fb601f061",
+    "url": "/Rest-Countries-API/static/js/2.facde861.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Rest-Countries-API/static/js/2.facde861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca5919db099b2822944",
+    "url": "/Rest-Countries-API/static/js/main.3b85bd1b.chunk.js"
+  },
+  {
+    "revision": "16701ab57ade981e4839",
+    "url": "/Rest-Countries-API/static/js/runtime-main.48daca13.js"
+  }
+]);
